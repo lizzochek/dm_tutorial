5087232554e6641bbf7ba42bb6ce43850655b31f
lizzochek <littled685@gmail.com>
2020-05-24 19:41:04 +0300

    Problem with JSON

--- main.js
+++ main.js
@@ -2,3 +2,21 @@
 
 const showLecture = require('./lib/doc_viewer.js');
 const showQuiz = require('./lib/quiz.js');
+
+const path = require('path').resolve(__dirname, './progress.json');
+const file = require('fs').readFileSync(path, 'utf8');
+const progress = JSON.parse(file);
+
+function show(arr) {
+    for(let obj of arr) {
+        console.log(obj);
+        if(obj.progress === "false") {
+            console.log(obj.progress);
+            if(!obj.isQuiz) {
+                await showLecture(obj.path);
+                obj.progress = true;
+            } else await showQuiz(obj.path);
+        }
+    }
+}
+show(progress);
