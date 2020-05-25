53def55cda96a23f421b508ba5314f5ffc787f73
lizzochek <littled685@gmail.com>
2020-05-25 17:43:02 +0300

    Modified

--- main.js
+++ main.js
@@ -2,18 +2,20 @@
 
 const child_process = require('child_process');
 
-const child = child_process.spawn('less', ['./lib/quiz.js'],
+const childDocViewer = child_process.spawn('node', ['./lib/doc_viewer.js'],
+ { shell: true });
+ const childQuizViewer = child_process.spawn('node', ['./quiz.js'],
  { shell: true });
 
-child.stdout.on('data', data => {
+ childDocViewer.stdout.on('data', data => {
   process.stdout.write(data);
 });
 
 process.stdin.on('data', data => {
-  child.stdin.write(`${data}\n`);
+  childDocViewer.stdin.write(`${data}\n`);
 });
 
-child.on('close', (code) => {
+childDocViewer.on('close', (code) => {
   console.log(`child process exited with code ${code}`);
   process.exit(0);
 });
